e07a30b6c27d3dc3b92a90ae41b5de4859b3138e
zvacet <ikoli@yahoo.com>
2015-10-31 23:35:55 +0100

    Create liblxqt_hr.ts

---
+++ translations/liblxqt_hr.ts
@@ -0,0 +1,188 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr_HR">
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../configdialog/lxqtconfigdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dijalog</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::MessageBox</name>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="70"/>
+        <source>LXQt Power Manager Error</source>
+        <translation>Greška LXQt upravitelja energijom</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="70"/>
+        <source>Hibernate failed.</source>
+        <translation>Hibernacija nije uspjela.</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::NotificationPrivate</name>
+    <message>
+        <location filename="../lxqtnotification.cpp" line="152"/>
+        <source>Notifications Fallback</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::PowerManager</name>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="112"/>
+        <source>Hibernate</source>
+        <translation>Hiberniraj</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="119"/>
+        <source>Suspend</source>
+        <translation>Suspendiraj</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="126"/>
+        <source>Reboot</source>
+        <translation>Ponovno podigni</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="133"/>
+        <source>Shutdown</source>
+        <translation>Isključi</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="140"/>
+        <source>Logout</source>
+        <translation>Odjava</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="152"/>
+        <source>LXQt Session Suspend</source>
+        <translation>Suspendiraj LXQt sesiju</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="153"/>
+        <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
+        <translation>Želite li doista suspendirati vaše računalo?&lt;p&gt;Suspendira računalo u stanje manje energije. Sustav se neće očuvati ako nestane energije.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="162"/>
+        <source>LXQt Session Hibernate</source>
+        <translation>Hiberniraj LXQt sesiju</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="163"/>
+        <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
+        <translation>Želite li doista hibernirati vaše računalo?&lt;p&gt;Hibernira računalo u stanje manje energije. Stanje sustava će biti očuvano ako nestane energije.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="172"/>
+        <source>LXQt Session Reboot</source>
+        <translation>LXQt sesija ponovno podizanje</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="173"/>
+        <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
+        <translation>Želite li doista ponovno pokrenuti vaše računalo? sav nespremljeni rad će biti izgubljen...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="182"/>
+        <source>LXQt Session Shutdown</source>
+        <translation>LXQt sesija isključi</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="183"/>
+        <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
+        <translation>Želite li doista isključiti vaše računalo? Sav nespremljeni rad će bit iizgubljen...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="192"/>
+        <source>LXQt Session Logout</source>
+        <translation>LXQt sesija odjava</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="193"/>
+        <source>Do you want to really logout? All unsaved work will be lost...</source>
+        <translation>Želite li se doista odjavti? sav nespremljeni rad će biti izgubljen...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="201"/>
+        <location filename="../lxqtpowermanager.cpp" line="206"/>
+        <source>LXQt Power Manager Error</source>
+        <translation>Greška LXQt upravitelja energijom</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="201"/>
+        <source>Hibernate failed.</source>
+        <translation>Hibernacija nije uspjela.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="206"/>
+        <source>Suspend failed.</source>
+        <translation>Suspenzija nije uspjela.</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::ScreenSaver</name>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="51"/>
+        <source>Lock Screen</source>
+        <translation>Zaključaj zaslon</translation>
+    </message>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="76"/>
+        <source>Screen Saver Error</source>
+        <translation>Greška čuvara zaslona</translation>
+    </message>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="77"/>
+        <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="84"/>
+        <location filename="../lxqtscreensaver.cpp" line="92"/>
+        <location filename="../lxqtscreensaver.cpp" line="101"/>
+        <source>Screen Saver Activation Error</source>
+        <translation>Greška pri aktivaciji čuvara zaslona</translation>
+    </message>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="85"/>
+        <source>An error occurred starting screensaver. Ensure you have xscreensaver installed and running.</source>
+        <translation>Došlo je do greške pri pokretanju čuvara zaslona. Provjerite da imatexscreensaver instaliran i pokrenut.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="93"/>
+        <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
+        <translation>Došlo je do greške prilikom pokretanja čuvara zaslona. Radnja&apos;aktiviraj&apos; nije uspjela. provjerite imate lixscreensaverinstaliran i pokrenut. </translation>
+    </message>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="102"/>
+        <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver: %1.</source>
+        <translation>Došlo je do pogreške prilikom pokretanja čuvara zaslona. nepoznata greška- nedokumentirana povratna vrijednost izxdg-screensavera: %1.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="91"/>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="141"/>
+        <source>Power Manager Error</source>
+        <translation>Greška upravitelja energijom</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="92"/>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="142"/>
+        <source>QDBusInterface is invalid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="106"/>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="156"/>
+        <source>Power Manager Error (D-BUS call)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
